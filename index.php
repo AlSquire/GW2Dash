6748ee978caa131477373d929604506a48fd52bc
AlSquire <alsquire@gmail.com>
2013-05-24 17:51:22 +0200

    Add dragon events states and auto refresh

--- index.php
+++ index.php
@@ -16,21 +16,39 @@
     <label for="world_select">World</label>
     <select id="world_select" ng-model="worldId" ng-options="world.id as world.name for world in worlds"></select>
 
+    <label for="interval_select">Refresh every</label>
+    <select id="interval_select" ng-model="interval">
+      <option value="10000">10s</option>
+      <option value="30000">30s</option>
+      <option value="60000">1min</option>
+      <option value="300000">5min</option>
+      <option value="0">... time I hit the refresh button</option>
+    </select>
+
     <section id="wvw">
       <h1>WvW</h1>
       <section>
-        <h2>Red world : {{redWorld.name}}</h2>
+        <h2>Red world: {{redWorld.name}}</h2>
         {{redWorld.score}}
       </section>
       <section>
-        <h2>Blue world : {{blueWorld.name}}</h2>
+        <h2>Blue world: {{blueWorld.name}}</h2>
         {{blueWorld.score}}
       </section>
       <section>
-        <h2>Green world : {{greenWorld.name}}</h2>
+        <h2>Green world: {{greenWorld.name}}</h2>
         {{greenWorld.score}}
       </section>
     </section>
+
+    <section id="watched_events">
+      <h1>Dragons & co</h1>
+      <section ng-repeat="e in watchedEvents" class="state_{{e.state.toLowerCase()}}">
+        <h2>{{e.name}}</h2>
+        {{e.state}}
+      </section>
+    </section>
+
   </div>
 </body>
 </html>
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ app.config(function($routeProvider, $locationProvider) {
   $routeProvider.when('/world/:worldId', { controller: 'gw2Ctrl' })
 });
 
-app.controller('gw2Ctrl', function ($scope, $http, $resource, $location, $route, $routeParams) {
+app.controller('gw2Ctrl', function($scope, $http, $resource, $location, $route, $routeParams, $timeout) {
   $http.defaults.useXDomain = true;
   // So CORS works event without OPTIONS requests supported by API server
   delete $http.defaults.headers.common['X-Requested-With'];
@@ -16,17 +16,26 @@ app.controller('gw2Ctrl', function ($scope, $http, $resource, $location, $route,
   Worlds = $resource('https://api.guildwars2.com/v1/world_names.json');
   Matches = $resource('https://api.guildwars2.com/v1/wvw/matches.json');
   MatchDetails = $resource('https://api.guildwars2.com/v1/wvw/match_details.json');
+  Events = $resource('https://api.guildwars2.com/v1/events.json');
+
+  var timer;
+  $scope.interval = 30000;
 
   $scope.worlds = Worlds.query();
   $scope.redWorld = {};
   $scope.blueWorld = {};
   $scope.greenWorld = {};
 
-  // When a world is selected...
-  $scope.$watch('worldId', function() {
+  $scope.watchedEvents = [
+    { id: "0464CB9E-1848-4AAA-BA31-4779A959DD71", name: "Claw of Jormag" },
+    { id: "568A30CF-8512-462F-9D67-647D69BEFAED", name: "Tequatl the Sunless" },
+    { id: "03BF176A-D59F-49CA-A311-39FC6F533F2F", name: "The Shatterer" }
+  ]
+
+  var fetch = function() {
     Matches.get({}, function(data) {
       // Loop through the matches to find the one in wich one the selected world is participating
-      for(i = 0; i < data.wvw_matches.length; i++) {
+      for (var i = 0; i < data.wvw_matches.length; i++) {
         m = data.wvw_matches[i];
         if (m.red_world_id == $scope.worldId || m.blue_world_id == $scope.worldId || m.green_world_id == $scope.worldId) {
           // Found!
@@ -41,17 +50,50 @@ app.controller('gw2Ctrl', function ($scope, $http, $resource, $location, $route,
           });
           
           // Retrieve the name of the three worlds in this match
-          for (j= 0; j < $scope.worlds.length; j++) {
+          for (var j = 0; j < $scope.worlds.length; j++) {
             w = $scope.worlds[j];
             if (m.red_world_id == w.id)   { $scope.redWorld.name = w.name; }
             if (m.blue_world_id == w.id)  { $scope.blueWorld.name = w.name; }
             if (m.green_world_id == w.id) { $scope.greenWorld.name = w.name; }
           }
+
+          Events.get({ world_id: $scope.worldId }, function(data) {
+            $scope.events = data.events;
+            for (var i = 0; i < $scope.events.length; i++) {
+              e = $scope.events[i];
+              for (var j = 0; j < $scope.watchedEvents.length; j++) {
+                we = $scope.watchedEvents[j];
+                if (e.event_id == we.id) {
+                  we.state = e.state;
+                }
+              }
+            }
+          });
+
           break;
         }
       }
-      // Update the uri so the page for the selected world can be refreshed and bookmarked
-      if (typeof $scope.worldId != 'undefined') { $location.path('/world/' + $scope.worldId); }
     });
+  }
+
+  var setTimer = function() {
+    $timeout.cancel(timer);
+    if ($scope.interval != "0") {
+      timer = $timeout(function() {
+        fetch();
+        setTimer();
+      }, $scope.interval);
+    }
+  }
+
+  // When a world is selected...
+  $scope.$watch('worldId', function() {
+    if (typeof $scope.worldId != 'undefined') {
+      fetch();
+      // Update the uri so the page for the selected world can be refreshed and bookmarked
+      $location.path('/world/' + $scope.worldId);
+    }
   });
+
+  $scope.$watch('interval', setTimer);
 });
